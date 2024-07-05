ef4923409074619a8b66e1a17db9a7f251f0c8af
MaximilianAichner <maximilian.aichner@crystal-design.com>
2024-07-05 15:09:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask_cors import CORS
 import os
 import openai
 import json
+import config
 
 app = Flask(__name__)
 CORS(app)
 
-openai.api_key = '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'
-
+openai.api_key = config.myopenkey
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
     if 'audio' not in request.files:
@@ -30,25 +30,14 @@ def transcribe():
         language = response.get('language', 'de')  # Default to 'de' if language is not provided
 
         # Construct the prompt
-        prompt = f"""
-        Bitte analysiere den folgenden Text und gib mir NUR ein JSON (schreibe nichts anderes als was unten gefragt ist!) zurück mit folgenden Informationen und lasse kein Detail bei den Transkriptionen aus!:
-        - "Transcript_D": Die Transkription auf Deutsch bzw. falls text=italienisch deutsche Übersetzung.
-        - "Transcript_I": Die Transkription auf Italienisch bzw. falls text=deutsch Übersetzung auf Italienisch.
-        - "Eindruck_D": Der Eindruck, den der Text auf Deutsch hinterlässt.
-        - "Eindruck_I": Der Eindruck, den der Text auf Italienisch hinterlässt.
-        - "Höflichkeit": Eine Zahl zwischen 0% und 100%, wobei 100% sehr höflich und 0% sehr frech ist.
-        - "Sympathisch": Eine Zahl zwischen 0% und 100%, wobei 100% sehr sympathisch und 0% sehr unsympathisch ist.
-        - "Lobend": Eine Zahl zwischen 0% und 100%, wobei 100% überhaupt nicht beleidigend und 0% sehr beleidigend ist.
-        - "Wortwahl": Eine Zahl zwischen 0% und 100%, wobei 100% eine sehr gute Wortwahl hat und 0% eine sehr schlechte Wortwahl hat.
-        Hier ist der transkribierte Text: {transcript}
-        """
+        
 
         # Send prompt to OpenAI's GPT-3.5-turbo
         completion_response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k",
+            model=config.modelname,
             messages=[
                 {"role": "system", "content": "Du bist ein hilfsbereiter Assistent."},
-                {"role": "user", "content": prompt}
+                {"role": "user", "content": config.prompt}
             ]
         )
 
